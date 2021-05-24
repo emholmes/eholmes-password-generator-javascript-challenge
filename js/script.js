ff1d58ef7c0600b7f760622d160eef536d13f161
Erin Holmes <e.holmes09@yahoo.com>
2021-05-24 14:04:48 -0500

    random number function and get password length validation

--- js/script.js
+++ js/script.js
@@ -8,17 +8,27 @@ function writePassword() {
 
   //passwordText.value = password;
 
-  var passwordLength = window.prompt("How many characters would you like your password to be? (min: 8, max: 128)");
+  var getPasswordLength = function() {
+    var passwordLength = window.prompt("How many characters would you like your password to be? (min: 8, max: 128)");
+    if (passwordLength < "8" && passwordLength > "128") {
+      return getPasswordLength();
+    }
+  }
+  getPasswordLength();
 
   var allowLowercase = window.confirm("Can your password contain lowercase characters?");
-    if (allowLowerCase) {
-    }
-
   var allowUppercase = window.confirm("Can your password contain uppercase characters?");
   var allowNumericChar = window.confirm("Can your password contain numbers?");
-  var allowSpecialChar = window.confirm("can your password contain special characters?");
+  var allowSpecialChar = window.confirm("Can your password contain special characters?");
 
-  
+  var randomNumber = function(max) {
+    return Math.floor(Math.random() * (max - 1));
+  }
+
+  //select lowercase letter
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var lowerLetter = alphabet[randomNumber(26)];
+  console.log(lowerLetter);
 
 }
 
