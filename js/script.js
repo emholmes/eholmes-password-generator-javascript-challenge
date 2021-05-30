104e38f1cb15923bfd5400a7a1174db78e8faf9a
Erin Holmes <e.holmes09@yahoo.com>
2021-05-30 11:07:41 -0500

    updated get statements to return values

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 var generateBtn = document.querySelector("#generate");
 
 // global variables
-var newPasswordArray;
 var passwordLength;
 var allowLowercase;
 var allowUppercase;
@@ -17,30 +16,26 @@ var randomNumber = function(min, max) {
 
 // function for each character option: lowercase, uppercase, numeric and special
 var getLowerChar = function() {
-  var lowerCaseChar = String.fromCharCode([randomNumber(97, 122)]);
-  newPasswordArray.push(lowerCaseChar);
+  return String.fromCharCode([randomNumber(97, 122)]);
 }
 
 var getUpperChar = function() {
-  var upperCaseChar = String.fromCharCode([randomNumber(65, 90)]);
-  newPasswordArray.push(upperCaseChar);
+  return String.fromCharCode([randomNumber(65, 90)]);
 }
 
 var getNumericChar = function() {
-  var numericChar = randomNumber(0, 9);
-  newPasswordArray.push(numericChar);
+  return randomNumber(0, 9);
 }
 
 var getSpecialChar = function() {
   var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-  var specialChar = specialCharacters[randomNumber(0, (specialCharacters.length - 1))];
-  newPasswordArray.push(specialChar);
+  return specialCharacters[randomNumber(0, (specialCharacters.length - 1))];
 }
 
 
 var generatePassword = function() {
   // initialize newPasswordArray as empty array
-  newPasswordArray = [];
+  var newPasswordArray = [];
   
   // prompt user for a password length between 8 and 128 characters
   var getPasswordLength = function() {
@@ -57,21 +52,22 @@ var generatePassword = function() {
   // ask user for character types to include in password
   // and add at least one of each character type allowed 
   var confirmCharOptions = function() {
+    debugger;
     allowLowercase = window.confirm("Can your password contain lowercase characters?");
     if (allowLowercase) {
-      getLowerChar();
+      newPasswordArray.push(getLowerChar());
     }
     allowUppercase = window.confirm("Can your password contain uppercase characters?");
     if (allowUppercase) {
-      getUpperChar();
+      newPasswordArray.push(getUpperChar());
     }
     allowNumericChar = window.confirm("Can your password contain numbers?");
     if (allowNumericChar) {
-      getNumericChar();
+      newPasswordArray.push(getNumericChar());
     }
     allowSpecialChar = window.confirm("Can your password contain special characters?");
     if (allowSpecialChar) {
-      getSpecialChar();
+      newPasswordArray.push(getSpecialChar());
     }
     // validate user selected at least one character type
     if (!allowLowercase && !allowUppercase && !allowNumericChar && !allowSpecialChar) {
@@ -87,27 +83,27 @@ var generatePassword = function() {
     switch(randomNumber(1, 4)) {
       case 1:
         if (allowLowercase) {
-          getLowerChar();
+          newPasswordArray.push(getLowerChar());
         }
         break;
       case 2: 
       if (allowUppercase) {
-        getUpperChar();
+        newPasswordArray.push(getUpperChar());
       }
         break;
       case 3:
         if (allowNumericChar) {
-          getNumericChar();
+          newPasswordArray.push(getNumericChar());
         }
         break;
       case 4: 
         if (allowSpecialChar) {
-          getSpecialChar();
+          newPasswordArray.push(getSpecialChar());
         }
         break;
       default: 
         if (allowSpecialChar) {
-          getSpecialChar();
+          newPasswordArray.push(getSpecialChar());
         }
         break;
     };
